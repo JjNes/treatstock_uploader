3797ad2201180e18915b125366bc2523908c9008
Egor <jnesapps@gmail.com>
2021-12-28 17:12:07 +0300

    csv reader

--- run.py
+++ run.py
@@ -1,7 +1,20 @@
-from treatstock import Treatstock
+import csv
 
+from treatstock import Treatstock
+from thingiverse import Thingiverse
 
 api = Treatstock()
 
 
-r = api.create_model([])
+#r = api.create_model([])
+
+
+tv = Thingiverse("PRIma")
+#tv.download()
+
+
+with open('task.csv', 'r', newline='') as csvfile:
+    task = csv.DictReader(csvfile, delimiter=',')
+    for row in task:
+        res = Thingiverse(row['username']).download(10)
+        print(res)        
--- thingiverse/api.py
+++ thingiverse/api.py
@@ -1,10 +1,7 @@
 import os
-import re
-import json
 import time
 import zipfile
 import requests
-import pickle
 
 
 class Thingiverse:
@@ -19,27 +16,39 @@ class Thingiverse:
     def download(self, delay: int = 0) -> bool:
         page = 1
         per_page = 20
-        url = f"https://api.thingiverse.com/users/{self.username}/search/?page={page}&per_page={per_page}&type=things&sort=newest"
-        r = self.s.get(url)
-        if r.status_code != 200:
-            return False
-        data = r.json()
-        files = []
-        for things in data['hits']:        
-            public_url = things['public_url']
-            model_name = things['name']
-            zip = self.s.get(public_url+"/zip")
-            if zip.status_code != 200:
-                continue
-            file_name = zip.url[zip.url.rfind("/")+1:]
-            file_path = "/".join([self.output, file_name])
-            open(file_path, 'wb').write(zip.content)
-            archive = zipfile.ZipFile(file_path, 'r')
-            archive.extractall(file_path.strip(".zip"))
-            archive.close()
-            os.remove(file_path)
-            file = {"title": model_name, "path": file_path.strip(".zip")}
-            files.append(file)
-            time.sleep(delay)
-        pickle.dump(files, open("/".join([self.output, "models.dat"]), 'wb'))
+
+        while True:
+            url = f"https://api.thingiverse.com/users/{self.username}/search/?page={page}&per_page={per_page}&type=things&sort=newest"
+            r = self.s.get(url)
+            if r.status_code != 200:
+                return False
+            data = r.json()
+            total = data['total']
+            for things in data['hits']:        
+                public_url = things['public_url']
+                model_name = things['name']
+                zip = self.s.get(public_url+"/zip")
+                if zip.status_code != 200:
+                    continue
+                print(f"Model '{model_name}' downloaded")
+                file_name = zip.url[zip.url.rfind("/")+1:]
+                file_path = "/".join(["models", self.username])
+                try:
+                    os.makedirs(file_path)
+                except OSError:
+                    pass
+
+                file_path = "/".join([file_path, file_name])
+                open(file_path, 'wb').write(zip.content)
+                archive = zipfile.ZipFile(file_path, 'r')
+                archive.extractall(file_path.strip(".zip"))
+                archive.close()
+                with open(file_path.strip(".zip")+"/name.txt", "wb") as t:
+                    t.write(model_name.encode('utf-8'))
+                os.remove(file_path)
+                time.sleep(delay)
+            # Exit
+            if page * per_page >= total:
+                break
+            page += 1
         return True
